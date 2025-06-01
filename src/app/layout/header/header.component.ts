ae24d87cd3a7d8a2a47949ed887bb8238f3c4445
OQueLucas <lucasestevam365@gmail.com>
2025-06-01 20:35:02 -0300

    feat: implement header and theme toggle components with dark mode support

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ThemeToggleComponent } from '@layout/theme-toggle/theme-toggle.component';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterLink, ThemeToggleComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
---
+++ src/app/layout/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { ThemeService } from '@layout/service/theme.service';
+import { SunIconComponent } from '../../shared/icons/sun-icon.component';
+import { MoonIconComponent } from '../../shared/icons/moon-icon.component';
+
+@Component({
+  selector: 'app-theme-toggle',
+  imports: [SunIconComponent, MoonIconComponent],
+  templateUrl: './theme-toggle.component.html',
+  styleUrl: './theme-toggle.component.scss',
+})
+export class ThemeToggleComponent {
+  protected themeService = inject(ThemeService);
+
+  protected theme = this.themeService.currentTheme;
+
+  toggle() {
+    this.themeService.toggleTheme();
+  }
+}
